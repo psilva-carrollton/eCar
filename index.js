cea3accae4a67d1bf3168c3f25ce4c978322ceee
psilva-carrollton <psilva@carrollton.org>
2025-04-07 17:50:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let map;
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = { lat: 27.9322, lng: -81.96 };
+  
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
@@ -11,17 +12,17 @@ async function initMap() {
 
   // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 16,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
 
   // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+  // const marker = new AdvancedMarkerElement({
+  //   map: map,
+  //   position: position,
+  //   title: "Uluru",
+  // });
 }
 
 initMap();
